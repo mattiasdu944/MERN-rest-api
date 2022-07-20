8c8cd1df116e851740d58f5da30e177546f1b90e
Mattias <duartemattias3@gmail.com>
2022-07-19 21:50:23 -0400

    Se realizo el CRUD de los Usuarios

--- src/controllers/Usuario.js
+++ src/controllers/Usuario.js
@@ -1,32 +1,34 @@
 import { connect } from "../database";
 
 
-//Enlista a todos los usuraios
+//LISTAR TODOS LOS PACIENTES
 export const getUsuarios = async (req, res) => {
     try {
         const connection = await connect()
-        const [rows] = await connection.query("SELECT * FROM usuarios;");
-        console.log(rows)
+        const [rows] = await connection.query("SELECT * FROM usuarios");
         res.json(rows);
-        
+
     } catch (error) {
-        console.log(error);
+        res.json('Error en el servidor: ' + error.message);
     }
 }
 
-
-//Obtiene a solo un usuario
+//LISTAR UN SOLO PACIENTE
 export const getUsuario = async (req, res) => {
-    const id = req.params.id;
-    const connection = await connect()
-    const [rows] = await connection.query("SELECT * FROM usuario WHERE id = ?", [id]);
-    console.log(rows[0]);
-    res.json(rows[0]);
+    try {
+        const id = req.params.id;
+        const connection = await connect()
+        const [rows] = await connection.query("SELECT * FROM usuarios WHERE id = ?", [id]);
+        console.log(rows[0]);
+        res.json(rows[0]);
+        
+    } catch (error) {
+        res.json('Error en el servidor: ' + error.message);
+    }
 }
 
-//Permite crear a un usuario
+//CREAR UN NUEVO PACIENTE
 export const createUsuario = async (req, res) => {
-  
     const nombres = req.body.nombres;
     const paterno = req.body.paterno;
     const materno = req.body.materno;
@@ -34,30 +36,46 @@ export const createUsuario = async (req, res) => {
     const password = req.body.password;
     const rol = req.body.rol;
 
-    const connection = await connect()
-    const [results] = await connection.query("INSERT INTO usuarios(nombres, paterno, materno, correo, password, rol) VALUES (?,?,?,?,?,?)",[nombres, paterno, materno, correo, password, rol]);
-    res.json({
-      ...req.body
-    });
+    try {
+        const connection = await connect()
+        const [results] = await connection.query("INSERT INTO usuarios(nombres, paterno, materno, correo, password, rol) VALUES (?,?,?,?,?,?,?)",[ nombres, paterno, materno, correo, password, rol]);
+        res.json({
+          ...req.body
+        });
+        
+    } catch (error) {
+        res.json('Error en el servidor:' + error.message);
+
+    }
 }
 
-//Elimina a solo un usuario de la base de datos
+//ELIMINAR UN PACIENTE
 export const deleteUsuario = async (req, res) => {
-    const id = req.params.id
-    const connection = await connect()
-    const results = await connection.query("DELETE FROM usuarios WHERE id = ?", [id]);
-    res.sendStatus(204);
+    try {
+        const id = req.params.id
+        const connection = await connect()
+        const results = await connection.query("DELETE FROM usuarios WHERE id = ?", [id]);
+        res.sendStatus(204);
+        
+    } catch (error) {
+        res.json('Error en el servidor:' + error.message);
+    }
 
 }
 
-//Actualiza los datos de un usuario
+//MODIFICAR UN PACIENTE
 export const updateUsuario = async (req, res) => {
-    const id = req.params.id;
-    const connection = await connect()
-    const results = await connection.query("UPDATE usuarios SET ? WHERE id = ?",[
-      req.body, id
-    ]);
-    res.sendStatus(204);
-}
 
+    try {
+        const id = req.params.id;
+        const connection = await connect()
+        const results = await connection.query("UPDATE usuarios SET ? WHERE id = ?",[
+          req.body, id
+        ]);
+        res.sendStatus(204);
+        
+    } catch (error) {
+        res.json('Error en el servidor:' + error.message);
+    }
+}
 
